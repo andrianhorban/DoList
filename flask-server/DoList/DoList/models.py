02e75dfd8e5bd97c33ceed3a35ca18655863413f
andrianhorban <horbanpiff@gmail.com>
2021-03-27 18:10:48 +0200

    model fix

--- flask-server/DoList/DoList/models.py
+++ flask-server/DoList/DoList/models.py
@@ -7,13 +7,20 @@ from sqlalchemy.orm import validates
 
 class Item(db.Model):
     id = db.Column(db.INTEGER, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.Text, unique=True, nullable=False)
     text = db.Column(db.Text, default='')
     slug = db.Column(db.String(128), unique=True, nullable=False)
     is_completed = db.Column(db.Boolean, default=False, nullable=False)
 
+    def __init__(self,title: str, text: str, slug: str, is_completed: bool):
+        self.text = text
+        self.title = title
+        self.slug = slug
+        self.is_completed = is_completed
+        print("Item created")
+
     @validates('title')
-    def validate_title(self, value):
+    def validate_title(self, key, value):
         if Item.query.filter(Item.title == value).first():
             raise AssertionError('Item with this title already exist')
         return value
@@ -29,15 +36,12 @@ class Item(db.Model):
     def __repr__(self):
         return f"{Item.title, Item.text, Item.is_completed}"
 
-    def __init__(self,title, text, slug, is_completed):
-        self.title = title
-        self.text = text
-        self.slug = slug
-        self.is_completed = is_completed
+
+    def save_db(self):
+        db.session.add(self)
+        db.session.commit()
 
     @staticmethod
     def slug_generator(target, value: str):
         target.slug = slugify(value)
 
-
-db.event.listen(Item.title, 'set', Item.slug_generator, retval=False)
