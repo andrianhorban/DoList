9f80a5b8e3c762bc8f393334282f0f98fc162e70
andrianhorban <horbanpiff@gmail.com>
2021-03-29 16:30:44 +0300

    working tests fix

--- flask-server/tests/test_unit.py
+++ flask-server/tests/test_unit.py
@@ -7,6 +7,7 @@ app = create_app('testing')
 
 
 def test_dolist_get_200():
+    """get test"""
     with app.test_client() as client:
         response = client.get('/api/dolist')
         print(response.status_code)
@@ -14,35 +15,35 @@ def test_dolist_get_200():
 
 
 def test_dolist_post_201():
+    """post test"""
     with app.test_client() as client:
         response = client.post('/api/dolist', json=dict(
-            title='koly1231',
+            title='kolyTEST',
             text='sdgldsgld',
-            slug='koly-title1231',
+            slug='koly-titleTEST',
             is_completed=True
         ))
         assert response.status_code == 201
 
 
-def test_dolist_delete_204():
-    with app.test_client() as client:
-        response = client.delete('/api/dolist?item_title=koly1231', json=dict(
-            title='koly1231',
-            text='sdgldsgld',
-            slug='koly-title1231',
-            is_completed=True
-        ))
-        print(response.status_code)
-        assert response.status_code == 204
-
-
 def test_dolist_put_202():
+    """put test"""
     with app.test_client() as client:
-        response = client.put('/api/dolist?item_title=title1', json=dict(
-            title='title1',
+        response = client.put('/api/dolist?item_title=kolyTEST', json=dict(
+            title='title',
             text='changed',
-            slug='koly-title12313',
+            slug='koly-titleTEST',
             is_completed=True
         ))
         print(response.status_code)
         assert response.status_code == 202
+
+
+def test_dolist_delete_204():
+    """delete test"""
+    with app.test_client() as client:
+        response = client.delete('/api/dolist?item_title=title')
+        print(response.status_code)
+        assert response.status_code == 204
+
+
