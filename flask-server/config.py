476d3ca447a7177b36b61835ecf4a57b3b9cad20
andrianhorban <horbanpiff@gmail.com>
2021-03-29 17:09:45 +0300

    added swagger

--- flask-server/config.py
+++ flask-server/config.py
@@ -1,10 +1,22 @@
 """Application configuration."""
 import os
 
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+
 
 class BaseConfig:
     """Base configuration."""
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    #swagger registration
+    APISPEC_SPEC = APISpec(
+        title='DoList',
+        version='v1',
+        plugins=[MarshmallowPlugin()],
+        openapi_version='2.0.0'
+    )
+    APISPEC_SWAGGER_URL = '/swagger/'
+    APISPEC_SWAGGER_UI_URL = '/swagger-ui/'
 
 
 class DevelopmentConfig(BaseConfig):
