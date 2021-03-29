494632fdaf0f5c56d6687273b46530a215c845ed
andrianhorban <horbanpiff@gmail.com>
2021-03-29 16:34:18 +0300

    more tests

--- flask-server/tests/test_unit.py
+++ flask-server/tests/test_unit.py
@@ -26,6 +26,31 @@ def test_dolist_post_201():
         assert response.status_code == 201
 
 
+def test_dolist_post_500():
+    """post test exists"""
+    with app.test_client() as client:
+        response = client.post('/api/dolist', json=dict(
+            title='kolyTEST',
+            text='sdgldsgld',
+            slug='koly-titleTEST',
+            is_completed=True
+        ))
+        assert response.status_code == 500
+
+
+def test_dolist_put_500():
+    """put test exists"""
+    with app.test_client() as client:
+        response = client.put('/api/dolist?item_title=kolyTEST', json=dict(
+            title='kolyTEST',
+            text='sdgldsgld',
+            slug='koly-titleTEST',
+            is_completed=True
+        ))
+        print(response.status_code)
+        assert response.status_code == 500
+
+
 def test_dolist_put_202():
     """put test"""
     with app.test_client() as client:
@@ -47,3 +72,9 @@ def test_dolist_delete_204():
         assert response.status_code == 204
 
 
+def test_dolist_delete_500():
+    """deleting not existing element"""
+    with app.test_client() as client:
+        response = client.delete('/api/dolist?item_title=title')
+        print(response.status_code)
+        assert response.status_code == 500
